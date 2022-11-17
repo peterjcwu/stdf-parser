86ad321986753498ac93e38c71cb983913ff517b
peterjcwu <wolf952@gmail.com>
2022-11-18 00:24:20 +0800

    change db schema

--- src/stdfparser/stdf_sub_embedded_doc.py
+++ src/stdfparser/stdf_sub_embedded_doc.py
@@ -1,14 +1,9 @@
 import re
 from collections import defaultdict
 from typing import List, Dict
-
-
 from .stdf_sub import StdfSub
 from . import DBConn
-
 from datetime import datetime
-from typing import Optional
-
 from bson import ObjectId
 
 
@@ -27,22 +22,19 @@ class MirRow(BaseRow):
 
 
 class PrrRow(BaseRow):
-    def __init__(self, d: dict, mir_id: ObjectId):
-        self.mir_id = mir_id
+    def __init__(self, d: dict):
         self.site: int = d["SITE_NUM"]
-        self.num_test: int = d["NUM_TEST"]
+        # self.num_test: int = d["NUM_TEST"]
         self.x: int = d["X_COORD"]
         self.y: int = d["Y_COORD"]
         self.sb: int = d["SOFT_BIN"]
         self.hb: int = d["HARD_BIN"]
-        self.part_id = int(d["PART_ID"].decode())
+        self.i = int(d["PART_ID"].decode())
 
 
 class PtrRow(BaseRow):
     def __init__(self, d: dict, tag: str):
         self.tag: str = tag
-        self.mir_id: Optional[ObjectId] = None
-        self.prr_id: Optional[ObjectId] = None
         self.t_num: int = d["TEST_NUM"]
         self.text: str = d["TEST_TXT"].decode()
         self.site: int = d["SITE_NUM"]
@@ -51,31 +43,34 @@ class PtrRow(BaseRow):
         self.hi_lim: float = d['HI_LIMIT']
         self.unit: str = d['UNITS'].decode()
 
-    def update_key(self, mir_id: ObjectId, prr_id: ObjectId):
-        self.mir_id = mir_id
-        self.prr_id = prr_id
-        return self
-
     def __repr__(self):
         return f"{self.t_num}:{self.text}:{self.v}"
 
 
-class StdfSubWlan(StdfSub):
+class StdfSubEmbeddedDoc(StdfSub):
     def __init__(self, db_conn: DBConn, stdf_path: str, rec_filter=None):
         StdfSub.__init__(self, stdf_path=stdf_path)
         self.db_conn = db_conn
         self._rec_filter = rec_filter
         self.mir_id = ObjectId()
         self.cur_tag = defaultdict(dict)  # site -> tag
-        self._die_id: int = 0
-        self._cache: Dict[int, List[PtrRow]] = defaultdict(list)
+        self._cache1: Dict[int, List[PtrRow]] = defaultdict(list)  # site -> ptrRow
+        self._cache2: Dict[str, Dict] = defaultdict(dict)  # text -> pid -> {prr: PrrRow, ptr: PtrRow, value_list: [{v, tag}]}
         self._previous_rec_name: str = ""
         self._handlers: dict = {
+            "Mir": self.mir_handler,
             "Ptr": self.ptr_handler,
             "Dtr": self.dtr_handler,
             "Prr": self.prr_handler,
-            "Mir": self.mir_handler,
+            "Mrr": self.mrr_handler,
         }
+
+    def mrr_handler(self):
+        print()
+        # should we get the finish time?
+        # upload to db
+        self.db_conn.collection_ptr.insert_many([d for d in self._cache2.values()], ordered=False)
+        self.db_conn.collection_mir.update_one({"_id": self.mir_id}, {"$set": {"finish_t": datetime.fromtimestamp(self.data["FINISH_T"])}})
 
     def rec_filter(self) -> bool:
         if self.cur_rec.name not in self._handlers.keys():
@@ -97,21 +92,21 @@ class StdfSubWlan(StdfSub):
         self._previous_rec_name = "Mir"
 
     def dtr_handler(self) -> None:
-        def log_handler(key: str, site_val: str) -> None:
-            if self._previous_rec_name != "Log":
-                self.cur_tag.clear()
-            for site, val in eval("{" + site_val + "}").items():
-                self.cur_tag[site][key] = val
-            self._previous_rec_name = "Log"
+        # reset
+        if self._previous_rec_name != "Dtr":
+            self.cur_tag.clear()
 
-        # Log
         text = self.data["TEXT_DAT"].decode()
         if m := re.search(r"Log:(?P<field>\w+)=\[(?P<site_val>.*)]", text):
-            log_handler(m.group("field"), m.group("site_val"))
+            field, site_val = m.group("field"), m.group("site_val")
+            for site, val in eval("{" + site_val + "}").items():
+                self.cur_tag[site][field] = val
+
+        self._previous_rec_name = "Dtr"
 
     def ptr_handler(self) -> None:
         site = self.data["SITE_NUM"]
-        self._cache[site].append(PtrRow(self.data, self._get_tag_str(site)))
+        self._cache1[site].append(PtrRow(self.data, self._get_tag_str(site)))
         self._previous_rec_name = "Ptr"
 
     def _get_tag_str(self, site: int) -> str:
@@ -126,14 +121,29 @@ class StdfSubWlan(StdfSub):
         return ";".join(tags)
 
     def prr_handler(self) -> None:
-        prr_row = PrrRow(self.data, self.mir_id)
+        prr_row = PrrRow(self.data)
         res = self.db_conn.collection_prr.insert_one(prr_row.as_dict())
-        prr_id = res.inserted_id
+        # prr_id = res.inserted_id
 
-        ptr_row_dicts = [c.update_key(self.mir_id, prr_id).as_dict()
-                         for c in self._cache.get(prr_row.site, [])]
+        # ptr_row_dicts = [c.update_key(self.mir_id, prr_id).as_dict()
+        #                  for c in self._cache1.get(prr_row.site, [])]
 
-        if len(ptr_row_dicts) > 0:
-            self.db_conn.collection_ptr.insert_many(ptr_row_dicts, ordered=True)
+        # if len(ptr_row_dicts) > 0:
+        #     self.db_conn.collection_ptr.insert_many(ptr_row_dicts, ordered=True)
+        # move from cache1 to cache2
+        assert prr_row.site in self._cache1.keys()
+        for ptr_row in self._cache1.pop(prr_row.site):
+            if ptr_row.text not in self._cache2.keys():
+                self._cache2[ptr_row.text] = {
+                    "mir_id": self.mir_id,
+                    "text": ptr_row.text,
+                    "hi_lim": ptr_row.hi_lim,
+                    "lo_lim": ptr_row.lo_lim,
+                    "t_num": ptr_row.t_num,
+                    "unit": ptr_row.unit,
+                    "rows": [{"v": ptr_row.v, "t": ptr_row.tag, **(prr_row.as_dict())}]
+                }
+            else:
+                self._cache2[ptr_row.text]["rows"].append({"v": ptr_row.v, "t": ptr_row.tag, **(prr_row.as_dict())})
 
         self._previous_rec_name = "Prr"
--- tests/test_stdf_sub_wlan.py
+++ tests/test_stdf_sub_wlan.py
@@ -1,12 +1,12 @@
 from stdfparser import StdfPub, DBConn, parse
-from stdfparser.stdf_sub_wlan import StdfSubWlan
+from stdfparser.stdf_sub_embedded_doc import StdfSubEmbeddedDoc
 
 
 def test_stdf_sub_wlan_sweep_success():
     stdf_path = r"C:\log\w224_wlan_sweep\wlan_sweep\stdf2\sweep_with_svc.stdf.gz"
     pub = StdfPub(stdf_path)
 
-    db_conn = DBConn("redfinch")
-    sub = StdfSubWlan(db_conn, stdf_path)
+    db_conn = DBConn("embedded")
+    sub = StdfSubEmbeddedDoc(db_conn, stdf_path)
 
     parse(pub, sub)
